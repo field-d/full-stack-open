e242892d94536dd483dc2e2544071ef9476b01df
David <124706055@umail.ucc.ie>
2026-05-23 16:10:00 +0100

    Part 1.2 Exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -28,9 +28,19 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-    <p>{props.parts[0].part} {props.parts[0].exercises}</p>
-    <p>{props.parts[1].part} {props.parts[1].exercises}</p>
-    <p>{props.parts[2].part} {props.parts[2].exercises}</p>
+    <Part part={props.parts[0]}/>
+    <Part part={props.parts[1]}/>
+    <Part part={props.parts[2]}/>
+    </> 
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+    <p>
+    {props.part.part} {props.part.exercises}
+    </p>
     </>
   )
 }
